4007a43aff6d3baff8ecb5d74f9a898642e6f622
GreatnessGrace <graceunstoppable7@gmail.com>
2025-01-17 12:23:42 +0530

    Refactor App component to implement routing for main pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+import Search from "./pages/Search";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import SignUp from "./pages/Signup";
+import Subscription from "./pages/Subscriptions";
+import VideoDetail from "./pages/VideoDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/watch/:videoId", element: <Watch /> },
+    { path: "/search", element: <Search /> },
+    { path: "/login", element: <Login /> },
+    { path: "/profile", element: <Profile /> },
+    { path: "*", element: <NotFound /> },
+    { path: "/signup", element: <SignUp /> },
+    { path: "/subscription", element: <Subscription /> },
+    // { path: "/video-detail", element: <VideoDetail video={sampleVideo} /> },
+   
+  ]);
+
+  return routes; // Ensure this is within a BrowserRouter
+};
 
 export default App;
